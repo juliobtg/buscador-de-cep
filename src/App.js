3a3c95861dc14f923e6f974ff698372ae31cca81
juliobtg <144383115+juliobtg@users.noreply.github.com>
2023-10-31 19:56:43 -0300

    enviando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'; 
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+import api from './services/Api';
 
 function App() {
+  const [input, setInput] = useState(''); 
+  const [cep, setCep] = useState({})
+
+
+
+  async function clique() {
+    if (input === '') {
+      alert('Buscador vazio');
+    } else {
+      try {
+        const response = await api.get(`/${input}/json`); 
+       setCep(response.data)
+       setInput("")
+      } catch (error) {
+        alert('Erro ao buscar '); 
+        setInput("")
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">BUSCADOR DE CEP</h1>
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="button" onClick={clique}>
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+  
+        <h2>CEP: {cep.cep}</h2>
+        <span>{cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade}</span>
+        
+      </main>
+      )}
+     
     </div>
   );
 }
 
 export default App;
+
+
+
+
